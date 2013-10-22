d501982f2f3b878d674fabd2734ddc6a8e829156
Jimmy <jimwong1023@gmail.com>
2013-10-21 23:51:39 -0700

    add contact controller and refactor test

--- spec/features/static_pages_feature_spec.rb
+++ spec/features/static_pages_feature_spec.rb
@@ -2,39 +2,61 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Rspec Overview" }
+
   describe 'Home page' do
-    it "should have the content 'Rspec Overview'" do
+    before do
       visit '/static_pages/home'
+    end
+
+    it "should have the content 'Rspec Overview'" do
       expect(page).to have_content('Rspec Overview')
     end
 
     it "should have title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Rspec Overview | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe 'Help page' do
-    it "should have the content 'Help'" do
+    before do
       visit '/static_pages/help'
+    end
+
+    it "should have the content 'Help'" do
       expect(page).to have_content('Help')
     end
 
     it "should have title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Rspec Overview | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe 'About page' do
-    it "should have the content 'About'" do
+    before do
       visit '/static_pages/about'
+    end
+
+    it "should have the content 'About'" do
       expect(page).to have_content('About')
     end
 
     it "should have title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Rspec Overview | About")
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe 'Contact page' do
+    before do
+      visit '/static_pages/contact'
+    end
+
+    it "should have the content 'Contact'" do
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have title 'Contact'" do
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
